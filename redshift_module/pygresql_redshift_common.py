d2f82b7d254adbccb5d99abe6529c5f85bfe752a
Vikas Kamath <vikas.jce@gmail.com>
2021-12-29 10:52:38 -0500

    Update the Module

---
+++ redshift_module/pygresql_redshift_common.py
@@ -0,0 +1,15 @@
+import pg
+
+
+def get_connection(host,port,db_name,user,password_for_user):
+    rs_conn_string = "host=%s port=%s dbname=%s user=%s password=%s" % (
+        host, port, db_name, user, password_for_user)
+
+    rs_conn = pg.connect(dbname=rs_conn_string)
+    rs_conn.query("set statement_timeout = 3600000")
+    return rs_conn
+
+
+def query(con,query):
+    res = con.query(query)
+    return res

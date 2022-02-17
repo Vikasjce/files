540913d7188936cebc7de664d13f6cf9bf13df5f
Vikas Kamath <vikas.jce@gmail.com>
2022-02-17 08:33:18 -0500

    Updated the statement timeout

--- redshift_module/pygresql_redshift_common.py
+++ redshift_module/pygresql_redshift_common.py
@@ -6,7 +6,6 @@ def get_connection(host,port,db_name,user,password_for_user):
         host, port, db_name, user, password_for_user)
 
     rs_conn = pg.connect(dbname=rs_conn_string)
-    rs_conn.query("set statement_timeout = 7200000")
     return rs_conn
 
 
